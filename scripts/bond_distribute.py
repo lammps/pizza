410919eec84a40ba775a1e31b358c67aa5ac1314
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2018-05-09 21:59:01 +0200

    Update bond_distribute.py
    large modifications to the code using numpy for the calculations - the goal is to speed up the computation

--- scripts/bond_distribute.py
+++ scripts/bond_distribute.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python 
+"""
+Script:  bond_distribute.py 
+Purpose: binned bond length distributions by bond type
+Syntax:  bond_distribute.py datafile nbin rmin rmax outfile files ...
+          datafile = lammps data file
+          nbin = # of bins per bond type
+          rmin = min expected bond length
+          rmax = max expected bond length
+          outfile = file to write stats to
+          files = series of dump files
+Example: bond_distribute.py pore.data 1000 1.5 1.85 bonds.out pore.dump.1
+Author:  Paul Crozier (Sandia)
 
-# Script:  bond_distribute.py 
-# Purpose: binned bond length distributions by bond type
-# Syntax:  bond_distribute.py datafile nbin rmin rmax outfile files ...
-#          datafile = lammps data file
-#          nbin = # of bins per bond type
-#          rmin = min expected bond length
-#          rmax = max expected bond length
-#          outfile = file to write stats to
-#          files = series of dump files
-# Example: bond_distribute.py pore.data 1000 1.5 1.85 bonds.out pore.dump.1
-# Author:  Paul Crozier (Sandia)
-
-# enable script to run from Python directly w/out Pizza.py
+enable script to run from Python directly w/out Pizza.py
+"""
 
 from argparse import ArgumentParser
 import numpy
 from dump import dump
 from data import data
-from math import sqrt
 
 # main script
 def compute_bond_distribution():
- parser = ArgumentParser(description='A python script to bond distribution')
+ """The function doing the actual calculation of the bond distribution"""
+ 
+ parser = ArgumentParser(description='A python script to compute bond distribution using pizza.py')
 
  parser.add_argument("input_data_file", help="The name of the lammps input data file")
  parser.add_argument("nbins", type=int, help="# of bins per bond type")
@@ -34,35 +36,33 @@ def compute_bond_distribution():
  args = parser.parse_args()
 
  dt = data(args.input_data_file)	
- nbins = args.nbins
- rmin = args.rmin
- rmax = args.rmax
  files = ' '.join(args.dump_files[:])
 
  # get the bonds from the data file
-
  bond = dt.get("Bonds")
  nbonds = len(bond)
  btype = numpy.asarray(bond[:][1], dtype=int) - 1
  iatom = numpy.asarray(bond[:][2], dtype=int) - 1
  jatom = numpy.asarray(bond[:][3], dtype=int) - 1 
 
- ntypes = 0
- for i in xrange(nbonds): ntypes = max(bond[i][1],ntypes)
- ntypes = int(ntypes)
+ ntypes = int(numpy.max(btype)) + 1
+ bin = numpy.zeros((args.nbins, ntypes), dtype=int)
+ 
  ncount = numpy.zeros(ntypes, dtype=int)
- bin = nbins * [0]
- for i in xrange(nbins): 
-   bin[i] = ntypes * [0] 
-
+ for itype in range(0, ntypes):
+  ncount[itype] = numpy.sum(btype==itype)
+ 
  # read snapshots one-at-a-time
  d = dump(files,0)
  d.map(1,"id",2,"type",3,"x",4,"y",5,"z")
 
+ nconfs = 0 
  while True:
    time = d.next()
    if time == -1:
      break
+     
+   nconfs += 1
    
    xprd = d.snaps[-1].xhi - d.snaps[-1].xlo
    yprd = d.snaps[-1].yhi - d.snaps[-1].ylo
@@ -86,28 +86,21 @@ def compute_bond_distribution():
   
    r = numpy.sqrt(delx*delx + dely*dely + delz*delz)
 
-   for i in xrange(nbonds):   
+   for itype in range(0, ntypes):
+    xx, hist_edge = numpy.histogram(r[btype == itype],bins=args.nbins, range=(args.rmin, args.rmax))
+    bin[:][itype] += xx
     
-     ibin = int(nbins*(r[i] - rmin)/(rmax - rmin) + 0.5)
-     if ((ibin >= 0) and (ibin <= nbins-1)): 
-       bin[ibin][btype[i]] += nbins
-       ncount[btype[i]] += 1
-     else:
-       print "Warning: bond distance outside specified range"
-       print "Bond type:", btype[i]+1
-       print "Bond number:", i
-   print time,    
+   print("{} ".format(time))    
       
- print
  print("Printing bond distance normalized distribution to {}".format(args.output_file))
     
  with open(args.output_file,"w") as fp:
-  rrange = rmax - rmin
-  for i in xrange(nbins):
-    print >>fp, rmin + rrange*float(i)/float(nbins),
-    for j in xrange(ntypes):
+  rrange = args.rmax - args.rmin
+  for i in range(0, args.nbins):
+    print >>fp, args.rmin + rrange*float(i)/float(args.nbins),
+    for j in range(0, ntypes):
       if (ncount[j] > 0):
-        print >>fp, float(bin[i][j])/float(ncount[j])/rrange,
+        print >>fp, float(bin[i][j])/float(ncount[j]*nconfs),
       else:
         print >>fp, 0.0,
     print >>fp
