68667d1af00b86dceb150b86dc86ee0ef3123e93
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2018-05-09 21:09:37 +0200

    Update bond_distribute.py

--- scripts/bond_distribute.py
+++ scripts/bond_distribute.py
@@ -14,100 +14,94 @@
 
 # enable script to run from Python directly w/out Pizza.py
 
-import sys
+from argparse import ArgumentParser
+import numpy
 from dump import dump
 from data import data
 from math import sqrt
-if not globals().has_key("argv"): argv = sys.argv
 
 # main script
+def compute_bond_distribution():
+ parser = ArgumentParser(description='A python script to bond distribution')
 
-if len(argv) < 7:
-  raise StandardError, \
-   "Syntax: bond_distribute.py datafile nbin rmin rmax outfile files ..."
+ parser.add_argument("input_data_file", help="The name of the lammps input data file")
+ parser.add_argument("nbins", type=int, help="# of bins per bond type")
+ parser.add_argument("rmin", type=float, help="min expected bond length")
+ parser.add_argument("rmax", type=float, help="max expected bond length")
+ parser.add_argument("output_file", help="The name of the file to write stats")
+ parser.add_argument("dump_files", nargs='+', help="series of dump files")
 
-dt = data(argv[1])	
-nbins = int(argv[2])
-rmin = float(argv[3])
-rmax = float(argv[4])
-outfile = argv[5]
-files = ' '.join(argv[6:])
+ args = parser.parse_args()
 
-# get the bonds from the data file
+ dt = data(args.input_data_file)	
+ nbins = args.nbins
+ rmin = args.rmin
+ rmax = args.rmax
+ files = ' '.join(args.dump_files[:])
 
-bond = dt.get("Bonds")
-nbonds = len(bond)
-btype = nbonds * [0]
-iatom = nbonds * [0]
-jatom = nbonds * [0]
-for i in xrange(nbonds):
-  btype[i] = int(bond[i][1] - 1)
-  iatom[i] = int(bond[i][2] - 1)
-  jatom[i] = int(bond[i][3] - 1)
+ # get the bonds from the data file
 
-ntypes = 0
-for i in xrange(nbonds): ntypes = max(bond[i][1],ntypes)
-ntypes = int(ntypes)
-ncount = ntypes * [0]
-bin = nbins * [0]
-for i in xrange(nbins): 
-  bin[i] = ntypes * [0] 
+ bond = dt.get("Bonds")
+ nbonds = len(bond)
+ btype = numpy.asarray(bond[:][1], dtype=int) - 1
+ iatom = numpy.asarray(bond[:][2], dtype=int) - 1
+ jatom = numpy.asarray(bond[:][3], dtype=int) - 1 
 
-# read snapshots one-at-a-time
+ ntypes = 0
+ for i in xrange(nbonds): ntypes = max(bond[i][1],ntypes)
+ ntypes = int(ntypes)
+ ncount = numpy.zeros(ntypes, dtype=int)
+ bin = nbins * [0]
+ for i in xrange(nbins): 
+   bin[i] = ntypes * [0] 
 
-d = dump(files,0)
-d.map(1,"id",2,"type",3,"x",4,"y",5,"z")
+ # read snapshots one-at-a-time
+ d = dump(files,0)
+ d.map(1,"id",2,"type",3,"x",4,"y",5,"z")
 
-while 1:
-  time = d.next()
-  if time == -1: break
+ while True:
+   time = d.next()
+   if time == -1:
+     break
    
-  xprd = d.snaps[-1].xhi - d.snaps[-1].xlo
-  yprd = d.snaps[-1].yhi - d.snaps[-1].ylo
-  zprd = d.snaps[-1].zhi - d.snaps[-1].zlo
+   xprd = d.snaps[-1].xhi - d.snaps[-1].xlo
+   yprd = d.snaps[-1].yhi - d.snaps[-1].ylo
+   zprd = d.snaps[-1].zhi - d.snaps[-1].zlo
   
-  d.unscale() 
-  d.sort()
-  x,y,z = d.vecs(time,"x","y","z")
-   
-  for i in xrange(nbonds):
+   d.unscale() 
+   d.sort()
+   x,y,z = d.vecs(time,"x","y","z")
   
-    delx = x[jatom[i]] - x[iatom[i]]
-    dely = y[jatom[i]] - y[iatom[i]]
-    delz = z[jatom[i]] - z[iatom[i]]
-        
-    if abs(delx) > 0.5*xprd:
-      if delx < 0.0:
-        delx += xprd
-      else:
-        delx -= xprd
-    if abs(dely) > 0.5*yprd:
-      if dely < 0.0:
-        dely += yprd
-      else:
-        dely -= yprd
-    if abs(delz) > 0.5*zprd:
-      if delz < 0.0:
-        delz += zprd
-      else:
-        delz -= zprd
-        
-    r = sqrt(delx*delx + dely*dely + delz*delz)
+   x = numpy.asarray(x)
+   y = numpy.asarray(y)
+   z = numpy.asarray(z)
+  
+   delx = x[jatom[:]] - x[iatom[:]]
+   dely = y[jatom[:]] - y[iatom[:]]
+   delz = z[jatom[:]] - z[iatom[:]]
+  
+   delx -= xprd*numpy.rint((x[jatom[:]] - x[iatom[:]])/xprd)
+   dely -= yprd*numpy.rint((y[jatom[:]] - y[iatom[:]])/yprd)
+   delz -= zprd*numpy.rint((z[jatom[:]] - z[iatom[:]])/zprd)
+  
+   r = numpy.sqrt(delx*delx + dely*dely + delz*delz)
+
+   for i in xrange(nbonds):   
     
-    ibin = int(nbins*(r - rmin)/(rmax - rmin) + 0.5)
-    if ((ibin >= 0) and (ibin <= nbins-1)): 
-      bin[ibin][btype[i]] += nbins
-      ncount[btype[i]] += 1
-    else:
-      print "Warning: bond distance outside specified range"
-      print "Bond type:", btype[i]+1
-      print "Bond number:", i
-  print time,    
+     ibin = int(nbins*(r[i] - rmin)/(rmax - rmin) + 0.5)
+     if ((ibin >= 0) and (ibin <= nbins-1)): 
+       bin[ibin][btype[i]] += nbins
+       ncount[btype[i]] += 1
+     else:
+       print "Warning: bond distance outside specified range"
+       print "Bond type:", btype[i]+1
+       print "Bond number:", i
+   print time,    
       
-print
-print "Printing bond distance normalized distribution to",outfile
+ print
+ print("Printing bond distance normalized distribution to {}".format(args.output_file))
     
-with open(outfile,"w") as fp:
+ with open(args.output_file,"w") as fp:
   rrange = rmax - rmin
   for i in xrange(nbins):
     print >>fp, rmin + rrange*float(i)/float(nbins),
@@ -117,3 +111,6 @@ with open(outfile,"w") as fp:
       else:
         print >>fp, 0.0,
     print >>fp
+
+if __name__ == "__main__":
+  compute_bond_distribution()
